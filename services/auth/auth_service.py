55bb66d9d433884195a40138d296373bfefc3514
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-05 16:15:00 +0700

    Implement authentication service with login and registration functionality

---
+++ services/auth/auth_service.py
@@ -0,0 +1,34 @@
+from services.auth.models.login_request import LoginRequest
+from services.auth.models.login_response import LoginResponse
+from services.auth.models.register_request import RegisterRequest
+from services.auth.helpers.authorization_helper import AuthorizationHelper
+from services.auth.helpers.user_helper import UserHelper
+from services.general.base_service import BaseService
+from services.general.models.success_response import SuccessResponse
+from utils.api_utils import ApiUtils
+
+
+class AuthService(BaseService):
+    SERVICE_URL = "http://127.0.0.1:8000"
+
+    def __init__(self, api_utils: ApiUtils) -> None:
+        super().__init__(api_utils)
+
+        self.authorization_helper = AuthorizationHelper(self.api_utils)
+        self.user_helper = UserHelper(self.api_utils)
+
+    def register_user(self, register_request: RegisterRequest) -> SuccessResponse:
+        response = self.authorization_helper.post_register(
+            data=register_request.model_dump()
+        )
+        return SuccessResponse(**response.json())
+
+    def login_user(self, login_request: LoginRequest) -> LoginResponse:
+        response = self.authorization_helper.post_login(data=login_request.model_dump())
+        return LoginResponse(**response.json())
+
+    # def create_random_student(self): # EXAMPLE
+    #     raise NotImplementedError
+
+    # def create_group_and_studen(self): # EXAMPLE
+    #     raise NotImplementedError
