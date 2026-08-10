1fb78b2825bc308eb8d4eaf7bee331e77330c237
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-10 17:15:14 +0700

    Add test for creating authorized teachers in TestTeacherContract

--- tests/test_teacher_contract.py
+++ tests/test_teacher_contract.py
@@ -1,5 +1,19 @@
-class Teacher:
-    def test_create_teacher(
-        self,
-    ):
-        pass
+import random
+
+from faker import Faker
+
+from services.university.helpers.teacher_helper import TeacherHelper
+from services.university.models.base_teacher import SubjectEnum
+from services.university.models.teacher_request import TeacherRequest
+
+faker = Faker()
+
+
+class TestTeacherContract:
+    def test_create_teacher_authorized(self, university_api_utils_admin):
+        teacher_helper = TeacherHelper(api_utils=university_api_utils_admin)
+        teacher = TeacherRequest(
+            first_name=faker.first_name(),
+            last_name=faker.last_name(),
+            subject=random.choice(list(SubjectEnum)),
+        )
