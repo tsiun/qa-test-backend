6c2a90e7004c3ce02fa779078a9c8aa2e2d619b4
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-10 17:14:46 +0700

    Add delete and put methods to GroupHelper and StudentHelper for group and student management

--- services/university/helpers/group_helper.py
+++ services/university/helpers/group_helper.py
@@ -19,3 +19,13 @@ class GroupHelper(BaseHelper):
         GROUP_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{group_id}/"
         response = self.api_utils.get(GROUP_ID_ENDPOINT)
         return response
+
+    def delete_group(self, group_id: int) -> requests.Response:
+        GROUP_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{group_id}/"
+        response = self.api_utils.delete(GROUP_ID_ENDPOINT)
+        return response
+
+    def put_group(self, group_id: int, json: dict) -> requests.Response:
+        GROUP_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{group_id}/"
+        response = self.api_utils.put(GROUP_ID_ENDPOINT, json=json)
+        return response
--- services/university/helpers/student_helper.py
+++ services/university/helpers/student_helper.py
@@ -20,3 +20,13 @@ class StudentHelper(BaseHelper):
         STUDENT_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{student_id}/"
         response = self.api_utils.get(STUDENT_ID_ENDPOINT)
         return response
+
+    def delete_student(self, student_id: int) -> requests.Response:
+        STUDENT_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{student_id}/"
+        response = self.api_utils.delete(STUDENT_ID_ENDPOINT)
+        return response
+
+    def put_student(self, student_id: int, json: dict) -> requests.Response:
+        STUDENT_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{student_id}/"
+        response = self.api_utils.put(STUDENT_ID_ENDPOINT, json=json)
+        return response
