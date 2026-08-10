e8324faf65b811960f31c5f134560b3dfeeecb76
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-10 17:14:34 +0700

    Add delete and put methods to TeacherHelper; define SubjectEnum in BaseTeacher model

--- services/university/helpers/teacher_helper.py
+++ services/university/helpers/teacher_helper.py
@@ -19,3 +19,13 @@ class TeacherHelper(BaseHelper):
         TEACHER_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{teacher_id}/"
         response = self.api_utils.get(TEACHER_ID_ENDPOINT)
         return response
+
+    def delete_teacher(self, teacher_id: int) -> requests.Response:
+        TEACHER_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{teacher_id}/"
+        response = self.api_utils.delete(TEACHER_ID_ENDPOINT)
+        return response
+
+    def put_teacher(self, teacher_id: int, json: dict) -> requests.Response:
+        TEACHER_ID_ENDPOINT = f"{self.ENDPOINT_PREFIX}/{teacher_id}/"
+        response = self.api_utils.put(TEACHER_ID_ENDPOINT, json=json)
+        return response
--- services/university/models/base_teacher.py
+++ services/university/models/base_teacher.py
@@ -1,4 +1,14 @@
+from enum import StrEnum
+
 from pydantic import BaseModel, ConfigDict
+
+
+class SubjectEnum(StrEnum):
+    MATEMATICS = "Mathematics"
+    PHYSICS = "Physics"
+    HISTORY = "History"
+    BIOLOGY = "Biology"
+    GEOGRAPHY = "Geography"
 
 
 class BaseTeacher(BaseModel):
