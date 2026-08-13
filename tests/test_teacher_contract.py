584f550f16cdd47db562196dbd8d5aa6d693008a
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-13 16:03:28 +0700

    Add tests for teacher creation with unauthorized and authorized access

--- tests/test_teacher_contract.py
+++ tests/test_teacher_contract.py
@@ -1,7 +1,9 @@
 import random
 
 from faker import Faker
+import requests
 
+from services.university import university_service
 from services.university.helpers.teacher_helper import TeacherHelper
 from services.university.models.base_teacher import SubjectEnum
 from services.university.models.teacher_request import TeacherRequest
@@ -10,10 +12,32 @@ faker = Faker()
 
 
 class TestTeacherContract:
-    def test_create_teacher_authorized(self, university_api_utils_admin):
+    def test_create_teacher_anonym(self, university_api_utils_anonym):
+        teacher_helper = TeacherHelper(api_utils=university_api_utils_anonym)
+        teacher = TeacherRequest(
+            first_name=faker.first_name(),
+            last_name=faker.last_name(),
+            subject=random.choice(list(SubjectEnum)),
+        )
+        teacher_response = teacher_helper.post_teacher(json=teacher.model_dump())
+
+        assert (
+            teacher_response.status_code == requests.status_codes.codes.unauthorized
+        ), (
+            f"Wrong status code. Actual: '{teacher_response.status_code}',"
+            f"bud expected: '{requests.status_codes.codes.ok}'"
+        )
+
+    def test_create_teacher_admin(self, university_api_utils_admin):
         teacher_helper = TeacherHelper(api_utils=university_api_utils_admin)
         teacher = TeacherRequest(
             first_name=faker.first_name(),
             last_name=faker.last_name(),
             subject=random.choice(list(SubjectEnum)),
+        )
+        teacher_response = teacher_helper.post_teacher(json=teacher.model_dump())
+
+        assert teacher_response.status_code == requests.status_codes.codes.created, (
+            f"Wrong status code. Actual: '{teacher_response.status_code}',"
+            f"bud expected: '{requests.status_codes.codes.ok}'"
         )
