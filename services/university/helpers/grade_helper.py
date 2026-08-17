eb610a70f0c4a6b2b1fcce18628a3f37221cbd3e
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-17 15:47:06 +0700

    Refactor GradeHelper methods to standardize parameter naming and improve API request handling

--- services/university/helpers/grade_helper.py
+++ services/university/helpers/grade_helper.py
@@ -9,27 +9,16 @@ class GradeHelper(BaseHelper):
     GRADE_STATS_ENDPOINT = f"{ENDPOINT_PREFIX}/stats/"
     GRADE_ID_ENDPOINT = f"{ENDPOINT_PREFIX}/{{grade_id}}/"
 
-    def post_grade(self, json: dict) -> requests.Response:
-        response = self.api_utils.post(self.ROOT_ENDPOINT, json=json)
+    def post_grade(self, data: dict) -> requests.Response:
+        response = self.api_utils.post(self.ROOT_ENDPOINT, data=data)
         return response
 
     def get_grades(self, params: dict | None = None) -> requests.Response:
-        response = self.api_utils.get(self.ROOT_ENDPOINT)
+        response = self.api_utils.get(self.ROOT_ENDPOINT, params=params)
         return response
 
-    def get_grades_stats(
-            self,
-            teacher_id: int | None = None,
-            student_id: int | None = None,
-            group_id: int | None = None,
-    ) -> requests.Response:
-        params = {
-            "teacher_id": teacher_id,
-            "student_id": student_id,
-            "group_id": group_id,
-        }
-        params = {k: v for k, v in params.items() if v is not None}
-        response = self.api_utils.get(self.GRADE_STATS_ENDPOINT, **params)
+    def get_grades_stats(self, params: dict | None = None) -> requests.Response:
+        response = self.api_utils.get(self.GRADE_STATS_ENDPOINT, params=params)
         return response
 
     def delete_grade(self, grade_id: int) -> requests.Response:
@@ -37,7 +26,7 @@ class GradeHelper(BaseHelper):
         response = self.api_utils.delete(grade_id_endpoint)
         return response
 
-    def put_grade(self, grade_id: int, json: dict) -> requests.Response:
+    def put_grade(self, grade_id: int, data: dict) -> requests.Response:
         grade_id_endpoint = self.GRADE_ID_ENDPOINT.format(grade_id=grade_id)
-        response = self.api_utils.put(grade_id_endpoint, json=json)
+        response = self.api_utils.put(grade_id_endpoint, data=data)
         return response
