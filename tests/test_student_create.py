de250cfcd8a90d67cf6a04fd18ce35e1ff3237e5
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-21 14:47:09 +0700

    Refactor logging statements in student creation tests to use Logger.step for consistency

--- tests/test_student_create.py
+++ tests/test_student_create.py
@@ -13,12 +13,12 @@ faker = Faker()
 
 class TestStudentCreate:
     def test_create_student_admin(self, university_api_utils_admin):
-        Logger.info("### Step 1. Create group")
+        Logger.step("### Step 1. Create group")
         university_service = UniversityService(api_utils=university_api_utils_admin)
         group = GroupRequest(name=faker.word())
         group_response = university_service.create_group(group_request=group)
 
-        Logger.info("### Step 2. Create student")
+        Logger.step("### Step 2. Create student")
         student = StudentRequest(
             first_name=faker.first_name(),
             last_name=faker.last_name(),
@@ -29,17 +29,23 @@ class TestStudentCreate:
         )
         student_response = university_service.create_student(student_request=student)
 
+        Logger.step("Check that the student is linked to the created group")
+
         assert student_response.group_id == group_response.id, (
             f"Wrong group id. Actual: '{student_response.group_id}',"
             f"but expected '{group_response.id}'"
         )
 
     def test_create_student_by_admin(self, student_data):
+        Logger.step("Check that the student is created by admin")
+
         assert student_data.id is not None, (
             f"The student wasn't create, Actual: 'None', but expected: '{student_data.id}'"
         )
 
     def test_create_student_in_group(self, group_data, student_data):
+        Logger.step("Check that the student is created in the group")
+
         assert student_data.group_id == group_data.id, (
             f"Wrong student id, Actual: '{student_data.group_id}', but expected: '{group_data.id}'"
         )
