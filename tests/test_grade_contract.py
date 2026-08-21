71568fef62269570fe8855f8a13412245851604e
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-21 14:46:24 +0700

    Refactor logging statements in test_create_grade_admin to use Logger.step for consistency

--- tests/test_grade_contract.py
+++ tests/test_grade_contract.py
@@ -18,13 +18,13 @@ faker = Faker()
 
 class TestGradeContract:
     def test_create_grade_admin(self, university_api_utils_admin):
-        Logger.info("### Step 1. Create group")
+        Logger.step("### Step 1. Create group")
         university_service = UniversityService(api_utils=university_api_utils_admin)
         group = GroupRequest(name=faker.word())
         group_response = university_service.create_group(group_request=group)
         group_id = group_response.id
 
-        Logger.info("### Step 2. Create student")
+        Logger.step("### Step 2. Create student")
         student = StudentRequest(
             first_name=faker.first_name(),
             last_name=faker.last_name(),
@@ -36,7 +36,7 @@ class TestGradeContract:
         student_response = university_service.create_student(student_request=student)
         student_id = student_response.id
 
-        Logger.info("### Step 3. Create a teacher")
+        Logger.step("### Step 3. Create a teacher")
         university_service = UniversityService(api_utils=university_api_utils_admin)
         teacher = TeacherRequest(
             first_name=faker.first_name(),
@@ -46,16 +46,16 @@ class TestGradeContract:
         teacher_response = university_service.create_teacher(teacher_request=teacher)
         teacher_id = teacher_response.id
 
-        Logger.info("### Step 4. Create a grade")
+        Logger.step("### Step 4. Create a grade")
         grade_helper = GradeHelper(api_utils=university_api_utils_admin)
         grade = GradeRequest(
             teacher_id=teacher_id,
             student_id=student_id,
             grade=random.randint(a=MIN_GRADE, b=MAX_GRADE),
-            group_id=group_id,
+            # group_id=group_id,
         )
 
-        response = grade_helper.post_grade(json=grade.model_dump())
+        response = grade_helper.post_grade(data=grade.model_dump())
 
         assert response.status_code == requests.status_codes.codes.created, (
             f"Wrong status code, Actual: '{requests.status_codes.codes.created}',"
