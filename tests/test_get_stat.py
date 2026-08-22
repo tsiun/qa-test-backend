8c7290df94b84e0e705f000a240fc7273f794303
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-22 15:11:56 +0700

    Refactor grade creation in test_get_stat_for_group for clarity and accuracy in statistics calculation

--- tests/test_get_stat.py
+++ tests/test_get_stat.py
@@ -78,29 +78,40 @@ class TestGetStat:
         )
 
         Logger.step("### Step 4. Create a grades for the student by teachers")
-        grade_1 = university_service.create_grade(
+        grade_student1_by_teacher1 = university_service.create_grade(
             grade_request=GradeRequest(
                 teacher_id=teacher_1.id,
                 student_id=student_1.id,
                 grade=random.randint(a=MIN_GRADE, b=MAX_GRADE),
             )
         )
-        grade_2 = university_service.create_grade(
+        grade_student1_by_teacher2 = university_service.create_grade(
             grade_request=GradeRequest(
                 teacher_id=teacher_2.id,
                 student_id=student_1.id,
                 grade=random.randint(a=MIN_GRADE, b=MAX_GRADE),
             )
         )
-        grade_3 = university_service.create_grade(
+        grade_student2_by_teacher2 = university_service.create_grade(
             grade_request=GradeRequest(
                 teacher_id=teacher_2.id,
                 student_id=student_2.id,
                 grade=random.randint(a=MIN_GRADE, b=MAX_GRADE),
             )
         )
+        # grade_student3_by_teacher1 = university_service.create_grade(
+        #     grade_request=GradeRequest(
+        #         teacher_id=teacher_1.id,
+        #         student_id=student_3.id,
+        #         grade=random.randint(a=MIN_GRADE, b=MAX_GRADE),
+        #     )
+        # )
         Logger.step("### Step 5. Count an expected average")
-        expected_avg = (grade_1.grade + grade_2.grade) / 2
+        expected_avg = (
+            grade_student1_by_teacher1.grade
+            + grade_student1_by_teacher2.grade
+            + grade_student2_by_teacher2.grade
+        ) / 3
 
         Logger.step("### Step 6. Get statistics for group_1")
         stats_group_1 = university_service.get_grades_stats(group_id=group_1.id)
@@ -113,18 +124,24 @@ class TestGetStat:
         )
 
         soft_assert.check(
-            stats_group_1.count == 2,
+            stats_group_1.count == 3,
             (
-                f"Wrong the total count of grades in group_1, Actual: '{stats_group_1.count}', bud expected '{2}'"
+                f"Wrong the total count of grades in group_1, Actual: '{stats_group_1.count}', but expected '{3}'"
             ),
         )
 
-        Logger.step("### Step 7. Get statustics for group_2 (empty)")
+        Logger.step("### Step 7. Get statistics for group_2 (empty)")
         stats_group_2 = university_service.get_grades_stats(group_id=group_2.id)
 
         soft_assert.check(
             stats_group_2.count == 0,
             (
-                f"Wrong the total coun of grades in group_2, Actual: '{stats_group_2.count}', but expected: '{0}'"
+                f"Wrong the total count of grades in group_2, Actual: '{stats_group_2.count}', but expected: '{0}'"
             ),
         )
+
+    def get_stat_for_student():
+        pass
+
+    def get_stat_for_teacher():
+        pass
