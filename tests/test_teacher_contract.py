367559dccba8c25719f1a910a338ebe241a8647c
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-27 15:13:00 +0700

    Enhance logging steps in teacher contract tests for better traceability

--- tests/test_teacher_contract.py
+++ tests/test_teacher_contract.py
@@ -3,6 +3,7 @@ import random
 from faker import Faker
 import requests
 
+from logger.logger import Logger
 from services.university import university_service
 from services.university.helpers.teacher_helper import TeacherHelper
 from services.university.models.base_teacher import SubjectEnum
@@ -13,6 +14,7 @@ faker = Faker()
 
 class TestTeacherContract:
     def test_create_teacher_anonym(self, university_api_utils_anonym):
+        Logger.step("### Step 1. Create a teacher anonymously")
         teacher_helper = TeacherHelper(api_utils=university_api_utils_anonym)
         teacher = TeacherRequest(
             first_name=faker.first_name(),
@@ -20,6 +22,8 @@ class TestTeacherContract:
             subject=random.choice(list(SubjectEnum)),
         )
         teacher_response = teacher_helper.post_teacher(json=teacher.model_dump())
+
+        Logger.step("### Step 2. Check that a teacher was not created")
 
         assert (
             teacher_response.status_code == requests.status_codes.codes.unauthorized
@@ -29,6 +33,7 @@ class TestTeacherContract:
         )
 
     def test_create_teacher_admin(self, university_api_utils_admin):
+        Logger.step("### Step 1. Create a teacher as an admin")
         teacher_helper = TeacherHelper(api_utils=university_api_utils_admin)
         teacher = TeacherRequest(
             first_name=faker.first_name(),
@@ -37,6 +42,7 @@ class TestTeacherContract:
         )
         teacher_response = teacher_helper.post_teacher(json=teacher.model_dump())
 
+        Logger.step("### Step 2. Check the response status code")
         assert teacher_response.status_code == requests.status_codes.codes.created, (
             f"Wrong status code. Actual: '{teacher_response.status_code}',"
             f"bud expected: '{requests.status_codes.codes.ok}'"
