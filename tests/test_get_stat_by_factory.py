d454ac6c4bd2178c8a02d22e03a425f3a7b1ad76
Yan Tsiun <tsiun4ick@gmail.com>
2026-08-27 15:12:27 +0700

    Refactor tests in TestGetStatBusinessLogic to use common entities and improve average calculation logic

--- tests/test_get_stat_by_factory.py
+++ tests/test_get_stat_by_factory.py
@@ -1,4 +1,5 @@
 from logger.logger import Logger
+from services.university.models.grade_stats_response import GradeStatsResponse
 from services.university.university_service import UniversityService
 
 
@@ -6,29 +7,20 @@ class TestGetStatBusinessLogic:
     def test_average_calculation(
         self,
         university_api_utils_admin,
-        teacher_factory,
-        student_factory,
-        group_factory,
-        grade_factory,
+        common_entities,
     ):
-        Logger.step("### Step 1. Prepare a group, teacher, and student")
+        Logger.step("### Step 1. Get common entities")
         university_service = UniversityService(api_utils=university_api_utils_admin)
-        group = group_factory()
-        teacher = teacher_factory()
-        student = student_factory(group_id=group.id)
+        group = common_entities["group"]
+        grades = common_entities["grades"]
 
-        Logger.step("### Step 2. Give exact grades for check the calculation")
-        grade_1 = grade_factory(teacher_id=teacher.id, student_id=student.id, grade=2)
-        grade_2 = grade_factory(teacher_id=teacher.id, student_id=student.id, grade=3)
-        grade_3 = grade_factory(teacher_id=teacher.id, student_id=student.id, grade=3)
+        Logger.step("### Step 2. Calculate an expected result")
+        expected_avg = sum(grade.grade for grade in grades) / len(grades)
 
-        Logger.step("### Step 3. Calculate an expected result")
-        expected_avg = (grade_1.grade + grade_2.grade + grade_3.grade) / 3
-
-        Logger.step("### Step 4. Get the stats")
+        Logger.step("### Step 3. Get the stats")
         stats = university_service.get_grades_stats(group_id=group.id)
 
-        Logger.step("### Step 5. Check a business logic")
+        Logger.step("### Step 4. Check a business logic")
         assert stats.avg == expected_avg, (
             f"Wrong calculating, Actual: '{stats.avg}', but expected: '{expected_avg}'"
         )
@@ -36,29 +28,17 @@ class TestGetStatBusinessLogic:
     def test_count_total_grades(
         self,
         university_api_utils_admin,
-        student_factory,
-        teacher_factory,
-        group_factory,
-        grade_factory,
+        common_entities,
     ):
-        Logger.step("### Step 1. Prepare a group, teacher, and student")
+        Logger.step("### Step 1. Get common entities")
         university_service = UniversityService(api_utils=university_api_utils_admin)
-        group = group_factory()
-        teacher = teacher_factory()
-        student = student_factory(group_id=group.id)
+        group = common_entities["group"]
+        expected_count = len(common_entities["grades"])
 
-        Logger.step("### Step 2. Give exact grades for check the calculation")
-        grade_1 = grade_factory(teacher_id=teacher.id, student_id=student.id, grade=3)
-        grade_2 = grade_factory(teacher_id=teacher.id, student_id=student.id, grade=4)
-        grade_3 = grade_factory(teacher_id=teacher.id, student_id=student.id, grade=5)
-
-        Logger.step("### Step 3. Set an expected count of grades")
-        expected_count = 3
-
-        Logger.step("### Step 4. Get the stats")
+        Logger.step("### Step 2. Get the stats")
         stats = university_service.get_grades_stats(group_id=group.id)
 
-        Logger.step("### Step 5. Check a business logic")
+        Logger.step("### Step 3. Check a business logic")
         assert stats.count == expected_count, (
             f"Wrong count of grades, Actual: '{stats.count}', but expected: '{expected_count}'"
         )
@@ -220,7 +200,15 @@ class TestGetStatBusinessLogic:
         Logger.step("### Step 2. Get the stats")
         stats = university_service.get_grades_stats(group_id=group.id)
 
+        Logger.step("### Step 3. Create an expected result")
+        expected_status = GradeStatsResponse(
+            count=0,
+            min=None,
+            max=None,
+            avg=None,
+        )
+
         Logger.step("### Step 3. Check a business logic")
-        assert stats.avg is None, (
-            f"Wrong calculating, Actual: '{stats.avg}', but expected: 'None'"
+        assert stats == expected_status, (
+            f"Wrong calculating in empty group, Actual: '{stats}', but expected: '{expected_status}'"
         )
